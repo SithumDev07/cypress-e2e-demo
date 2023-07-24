1cd6595fc3055e7837d522d92495997c98c1f3dc
Sithum Basnayaka <sithum.dashantha@gmail.com>
2023-07-25 00:22:01 +0530

    created cypress test file for class creation

---
+++ client/cypress/e2e/class-creation/class-creation.cy.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+import Chance from "chance";
+
+const chance = new Chance();
+
+describe('Create class', () => {
+
+    describe('Select instructor', function () {
+
+    });
+
+})
